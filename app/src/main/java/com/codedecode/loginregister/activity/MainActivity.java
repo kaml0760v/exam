c251d4f8236931a1252cfb4036dfd0bacdbe2a16
kaml0760v <kamleshmaniya0@gmail.com>
2021-03-07 09:33:40 +0530

    first commit login and firebase data successful

--- app/src/main/java/com/codedecode/loginregister/activity/MainActivity.java
+++ app/src/main/java/com/codedecode/loginregister/activity/MainActivity.java
@@ -1,6 +1,10 @@
 package com.codedecode.loginregister.activity;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
@@ -9,8 +13,11 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -19,6 +26,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.codedecode.loginregister.R;
 import com.codedecode.loginregister.helper.SQLiteHandler;
@@ -44,18 +53,22 @@ public class MainActivity extends Activity {
 	private SQLiteHandler db;
 	private SessionManager session;
 	private Context mContext;
-	private TextView textView;
+	private TextView textView,textView2;
 	float weight  = 0 ;
+	final int DROP_RATE = 50;
+	final int DROP_FACTOR = 20;
+	Boolean alert = true;
+
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-//		TextView txtName = findViewById(R.id.name);
-//		TextView txtEmail = findViewById(R.id.email);
 		Button btnLogout = findViewById(R.id.btnLogout);
 		textView = findViewById(R.id.text1);
+		textView2 = findViewById(R.id.text2);
 		// SqLite database handler
 		db = new SQLiteHandler(getApplicationContext());
 
@@ -63,6 +76,7 @@ public class MainActivity extends Activity {
 		session = new SessionManager(getApplicationContext());
 
 		if (!session.isLoggedIn()) {
+//			calliing logout function if session time out
 			logoutUser();
 		}
 
@@ -77,6 +91,7 @@ public class MainActivity extends Activity {
 //		txtEmail.setText(email);
 
 		// Logout button click event
+//		event declaration for logout button
 		btnLogout.setOnClickListener(new View.OnClickListener() {
 
 			@Override
@@ -95,16 +110,21 @@ public class MainActivity extends Activity {
 //		mRelativeLayout = (RelativeLayout) findViewById(R.id.rl);
 //		mButton = (Button) findViewById(R.id.btn);
 		mImageView = (ImageView) findViewById(R.id.iv);
-
+		// getting reference of firebase database in db1
 		db1= FirebaseDatabase.getInstance().getReference().child("testing");
+		//event for fetching weight data from the FireBase
 		db1.addValueEventListener(new ValueEventListener() {
+			//overridden method if value change in realtime database
 			@Override
 			public void onDataChange(@NonNull DataSnapshot snapshot) {
 				for(DataSnapshot snapshot1:snapshot.getChildren())
 				{
-					String name = snapshot1.getValue().toString();
-					weight = Float.parseFloat(name);
-
+					//storing the weight of saline in variable
+					String weight_value = snapshot1.getValue().toString();
+					//converting the weight in float
+					weight = Float.parseFloat(weight_value);
+					//subtracting the IV tube and angel weight
+					weight = (float) (weight-0.017-0.014);
 					// Initialize a new Bitmap
 					Bitmap bitmap = Bitmap.createBitmap(
 							350, // Width
@@ -123,13 +143,13 @@ public class MainActivity extends Activity {
 					paint.setStyle(Paint.Style.FILL);
 					paint.setColor(Color.CYAN);
 					paint.setAntiAlias(true);
-
+					//initializing the border of paint
 					Paint paintstroke = new Paint();
 					paintstroke.setStyle(Paint.Style.STROKE);
 					paintstroke.setColor(Color.BLUE);
 					paintstroke.setStrokeWidth(10);
 
-
+					// Initialize a new Paint instance to draw the rounded rectangle
 					Paint painttwo = new Paint();
 					paint.setStyle(Paint.Style.FILL);
 					painttwo.setColor(Color.WHITE);
@@ -156,7 +176,8 @@ public class MainActivity extends Activity {
 							canvas.getWidth() - offset, // right
 							canvas.getHeight() - offset // bottom
 					);
-					int offsettwo = 50;
+
+					// Initialize a new RectF instance
 					RectF rectFtwo = new RectF(
 							offset, // left
 							offset, // top
@@ -184,7 +205,7 @@ public class MainActivity extends Activity {
 							cornersRadius, // ry
 							paint // Paint
 					);
-
+					// draw the second rounded corner rectangle of white color
 					canvas.drawRoundRect(
 							rectFtwo, // rect
 							cornersRadius, // rx
@@ -197,8 +218,25 @@ public class MainActivity extends Activity {
 					mImageView.setImageBitmap(bitmap);
 					mImageView.setImageBitmap(bitmap);
 					float temp = (weight*500)/535;
-					Log.d("hello","weight is "+String.format("%.2f",temp));
+					// calculation for the estimated time
+					int ESTIMATE_TIME = (int)temp/(DROP_RATE/DROP_FACTOR);
+					//displaying the estimate time in textbox
+					textView2.setText("Saline Bottle will be finish in\n "+String.valueOf(ESTIMATE_TIME)+" Minutes");
+
+					//Log.d("hello","weight is "+String.format("%.2f",temp));
+					//displaying the available fluid in textbox
 					textView.setText(String.valueOf(String.format("%.2f",temp))+" ML");
+					// condition for notification
+					if(temp <= 125 && alert == true){
+						gen_Notification();
+						if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.O)
+                        {
+                            NotificationChannel channel = new NotificationChannel("Custom Notification","Custom Notification",NotificationManager.IMPORTANCE_DEFAULT);
+                            NotificationManager manager = (NotificationManager)getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+                            manager.createNotificationChannel(channel);
+                        }
+						alert = false;
+					}
 				}
 			}
 
@@ -210,13 +248,44 @@ public class MainActivity extends Activity {
 
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menus,menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+		int id = item.getItemId();
+		if(id ==R.id.logout)
+		{
+			logoutUser();
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	// function for the notification generation
+	private void gen_Notification() {
+	    Log.d("hello","notification");
+		NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this,"Custom Notification");
+		// title of the notification
+		builder.setContentTitle("Help");
+		//content of the notification
+		builder.setContentText("Please visit Bed No. 1");
+		//setting the Icon of notification
+		builder.setSmallIcon(R.drawable.logo_round);
+		builder.setAutoCancel(true);
+
+        NotificationManagerCompat manager = NotificationManagerCompat.from(MainActivity.this);
+        manager.notify(1,builder.build());
+
+	}
+
 	/**
 	 * Logging out the user. Will set isLoggedIn flag to false in shared
 	 * preferences Clears the user data from sqlite users table
 	 * */
 	private void logoutUser() {
 		session.setLogin(false);
-
 		db.deleteUsers();
 
 		// Launching the login activity
